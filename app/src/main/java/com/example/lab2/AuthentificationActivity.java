a52f9efbeede184fe79bcb27320dfdc38613f59b
Cantagrel <mathieu.cantagrel@efrei.net>
2021-02-03 08:39:29 +0100

    'exercise3

--- app/src/main/java/com/example/lab2/AuthentificationActivity.java
+++ app/src/main/java/com/example/lab2/AuthentificationActivity.java
@@ -3,6 +3,15 @@ package com.example.lab2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class AuthentificationActivity extends AppCompatActivity {
 
@@ -10,5 +19,37 @@ public class AuthentificationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentification);
+        findViewById(R.id.AuthButton).setOnClickListener(v->{
+            URL url = null;
+            try {
+                url = new URL("http://www.android.com/");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                    String s = readStream(in);
+                    Log.i("JFL", s);
+                } finally {
+                    urlConnection.disconnect();
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private String readStream(InputStream is) {
+        try {
+            ByteArrayOutputStream bo = new ByteArrayOutputStream();
+            int i = is.read();
+            while(i != -1) {
+                bo.write(i);
+                i = is.read();
+            }
+            return bo.toString();
+        } catch (IOException e) {
+            return "";
+        }
     }
 }
