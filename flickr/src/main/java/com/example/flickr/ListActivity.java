62097448374580968ee98f011de9dec594c42034
Cantagrel <mathieu.cantagrel@efrei.net>
2021-02-10 14:09:46 +0100

    exercice 25

--- flickr/src/main/java/com/example/flickr/ListActivity.java
+++ flickr/src/main/java/com/example/flickr/ListActivity.java
@@ -3,18 +3,32 @@ package com.example.flickr;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutionException;
+
 public class ListActivity extends AppCompatActivity {
+
+    MyAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-        MyAdapter adapter = new MyAdapter();
+        adapter = new MyAdapter(getBaseContext());
 
         ListView listView = (ListView) findViewById(R.id.List);
         listView.setAdapter(adapter);
+
+        AsyncFlickrDl();
+    }
+
+    protected void AsyncFlickrDl(){
+        new AsyncFlickrJSONDataForList(adapter).execute("https://www.flickr.com/services/feeds/photos_public.gne?tags=trees&format=json");
     }
 }
--- flickr/src/main/java/com/example/flickr/MyAdapter.java
+++ flickr/src/main/java/com/example/flickr/MyAdapter.java
@@ -1,19 +1,30 @@
 package com.example.flickr;
 
+import android.content.Context;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.Vector;
 
 public class MyAdapter extends BaseAdapter {
 
     Vector<String> vector;
+    Context context;
+
+    public MyAdapter(Context context) {
+        this.vector = new Vector<>();
+        this.context = context;
+    }
 
     @Override
     public int getCount() {
-        return 0;
+        return vector.size();
     }
 
     @Override
@@ -28,11 +39,19 @@ public class MyAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        Log.i("JFL", "TODO: ");
-        return null;
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(context);
+            convertView = inflater.inflate(R.layout.text_view_layout, parent, false);
+        }
+
+        TextView textView = (TextView) convertView.findViewById(R.id.textView);
+        textView.setText(vector.get(position));
+
+        return convertView;
     }
 
-    public void dd(String url){
+    public void add(String url){
         vector.add(url);
+        Log.i("JFL", "Adding to adapter url : " + url);
     }
 }
